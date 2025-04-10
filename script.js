f94d9816b2982fa1f6736da0f79d6f5cb35e5de5
Anshul <avenger720720@gmail.com>
2025-04-10 14:31:16 +0530

    completed

--- script.js
+++ script.js
@@ -1,18 +1,29 @@
 const form = document.querySelector("form");
-const user1Input = document.querySelector("input #user-1");
-const user2Input = document.querySelector("input #user-2");
+const user1Input = document.querySelector("input#user-1");
+const user2Input = document.querySelector("input#user-2");
+const p = document.querySelector('p')
 
 form.addEventListener('submit', (e) => {
     e.preventDefault()
-    const [dateString, timeString] = user1Input.value.split(' ')
-    const [user1Day, user1Month, user1Year] = dateString.split('/').map((el) => Number(el))
-    const [user1Hour, user1Minute] = timeString.split(':').map((el) => Number(el))
-    const user1DOB = new Date(user1Year ,user1Month-1, user1Day, user1Hour, user1Minute)
+   const user1AgeInMs = Date.now() - parseDateString(user1Input.value).getTime()
+   const user2AgeInMs = Date.now() - parseDateString(user2Input.value).getTime()
+
+
+  
+  if(user1AgeInMs > user2AgeInMs) {
+    p.innerText = 'User 1 is older than user 2'
+  } else if (user1AgeInMs < user2AgeInMs) {
+    p.innerText = 'User 2 is older than user 21'
+  } else {
+    p.innerText = 'Both are of same age'
+  }
 })
 
-function parseDateString(dateString) {
-    const [dateString, timeString] = user1Input.value.split(' ')
-    const [user1Day, user1Month, user1Year] = dateString.split('/').map((el) => Number(el))
-    const [user1Hour, user1Minute] = timeString.split(':').map((el) => Number(el))
-    const user1DOB = new Date(user1Year ,user1Month-1, user1Day, user1Hour, user1Minute)
+function parseDateString(dobString) {
+    const [dateString, timeString] = dobString.split(' ')
+    const [Day, Month, Year] = dateString.split('/').map((el) => Number(el))
+    const [Hour, Minute] = timeString.split(':').map((el) => Number(el))
+    return new Date(Year ,Month-1, Day, Hour, Minute)
+
+    
 }
